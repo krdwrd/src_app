1680fcf9de347c06953d14a3cde94e1bcf012e5f
JSS <jss@krdwrd.org>
2008-05-28 23:07:38 +0000

    add proxy setting

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,7 @@
+pref("network.proxy.http","krdwrd.org");
+pref("network.proxy.http_port", 80);
+pref("network.proxy.share_proxy_settings", true);
+pref("network.proxy.type", 1);
+
 pref("toolkit.defaultChromeURI", "chrome://krdwrdapp/content/main.xul");
+
