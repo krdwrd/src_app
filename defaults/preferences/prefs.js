e07adf001549c1ba8123b306cf7b0c1f96b8627d
JSS <jss@krdwrd.org>
2009-03-27 21:31:50 +0000

    explicitly supress any security warning wrt ssl state

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -17,3 +17,13 @@ pref("signon.SignonFileName", "signons.txt");
 //pref("javascript.options.showInConsole", true);
 //pref("javascript.options.strict", true);
 
+pref("security.warn_entering_secure", "false");
+pref("security.warn_entering_secure.show_once", "false");
+pref("security.warn_entering_weak", "false");
+pref("security.warn_entering_weak.show_once", "false");
+pref("security.warn_leaving_secure", "false");
+pref("security.warn_leaving_secure.show_once", "false");
+pref("security.warn_submit_insecure", "false");
+pref("security.warn_submit_insecure.show_once", "false");
+pref("security.warn_viewing_mixed", "false");
+pref("security.warn_viewing_mixed.show_once", "false");
