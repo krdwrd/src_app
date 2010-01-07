5fa6fe534a9524772b072375fa47696d889a3c9e
Egon W. Stemle <egon@krdwrd.org>
2010-01-07 10:25:26 +0000

    trap for cleanup on INT; logic for non-concurrent execution

--- harvest.sh
+++ harvest.sh
@@ -1,52 +1,106 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # takes: list of files of url lists (ending in .ggx)
 
 export LANG=en_US.UTF-8
 RETRIES=3
 
-for g in $@;
-do
-    # sanity check
-    [[ $( diff -q <(sort -u ${g}) <(sort ${g}) ) ]] && echo "${g} has duplicate lines - omitting." && break
+function cleanup
+{
+    if [ -n ${FN} ]
+    then
+        echo cleaning up...
+        if [ -e ${LOG} ]; then rm -v ${LOG}; fi
+        if [ -e ${FN} ]; then rm -v ${FN}; fi
+        if [ -e ${FN}.awk ]; then rm -v ${FN}.awk; fi
+        if [ -e ${FN}.lock ]; then rmdir -v ${FN}.lock; fi
+    fi
+    exit
+}
 
+function settrap
+{
+    trap cleanup INT TERM
+}
+
+for g in $@
+do
     echo $g
+    # sanity check
+    if [ $(sort ${g} | uniq -d) ]
+    then 
+        echo "...has duplicate lines - omitting."
+        break
+    fi
+
     i=0
-    for url in `cat $g`;
+    cat=$(basename $g .ggx)
+    
+    # build a list of already processed files for later look-up
+    processed=
+    for num in \
+        $(sort \
+        <(seq -f "%05g" 1 $(( $(wc -l $g | cut -d ' ' -f1) )) ) \
+        <(ls -1 ${cat}.*.html 2>/dev/null | sed -e "s#${cat}\.##" -e 's#\.html##') | uniq -d | sed -e 's#^0\+##')
     do
-        echo "***************"
-        cat=`basename $g .ggx`
-        ind=`echo $i | awk '{ printf("%05d", $1);}'`
+        processed[${num}]=y
+    done
+
+    settrap
+
+    for url in $(cat $g)
+    do
         let i++
+
+        # look up current file in built list - and skip if already seen...
+        [ -n "${processed[${i}]}" ] && echo -n "." && continue
+
+        printf -v ind "%05d" $i
+        echo -e "\n***************"
         echo "COR: $cat"
         echo "IND: $ind"
         FN=$cat.$ind.html
         LOG=$cat.$ind.log
 
         # skip if file exists
-        if [[ -f $FN ]]; then
-            echo "DATE: "`date` >> $LOG
+        # skip if too many tries
+        # create lock while processing
+        if [[ -f $FN ]]
+        then
+            echo "DATE: "$(date) >> $LOG
             echo "EXISTS" >> $LOG
             echo "EXISTS"
             continue
-        elif [[ -f $LOG && $(grep -cE "^FAILED$" ${LOG}) -ge ${RETRIES} ]]; then 
+        elif [[ -f $LOG && $(grep -cE "^FAILED$" ${LOG}) -ge ${RETRIES} ]]
+        then 
             echo "CAPITULATED"
             continue
         else
-            # rm -f $LOG 2> /dev/null
-            echo "DATE: "`date` >> $LOG
+            if mkdir ${FN}.lock
+            then
+                # rm -f $LOG 2> /dev/null
+                echo "DATE: "$(date) >> $LOG
+            else
+                # echo "${FN} locked"
+                continue
+            fi
         fi
         
+        URL=$(awk '/URL:/ { print $2; }' $LOG)
+        EURL=$(echo $URL | sed 's/^\(.*\/\).*$/\1/' | sed 's/\//\\\\\//g')
+
         # download
-        $(dirname $0)/grabf.sh "$url" `pwd`/$cat.$ind 2>&1 >> $LOG
-        if [[ ! -f $FN ]]; then
+        $(dirname $0)/grabf.sh "$url" $(pwd)/$cat.$ind 2>&1 >> $LOG
+
+        # remove lock
+        rmdir ${FN}.lock
+        
+        if [[ ! -f $FN ]]
+        then
             echo "FAILED" >> $LOG
             echo "FAILED"
-            continue;
+            continue
         fi
-        
-        URL=`awk '/URL:/ { print $2; }' $LOG`
-        EURL=`echo $URL | sed 's/^\(.*\/\).*$/\1/' | sed 's/\//\\\\\//g' `
 
         # SKIP: determine encoding
         # 1. try content-type in html
@@ -72,9 +126,8 @@ do
         # split pre tags into single lines
         mv $FN $FN.awk
         awk '/<pre>/,/<\/pre>/ { gsub("$", "</pre><pre>"); } {print}' $FN.awk > $FN
-        rm $FN.awk
-		echo "DONE" >> $LOG
-		echo "DONE"
-
+        rm ${FN}.awk
+        echo "DONE" >> $LOG
+        echo "DONE"
     done
 done
